5caea597a30c0fc2e1a696628fff8332cd881e39
MateusLima04 <mateuslimaa04@gmail.com>
2026-04-12 23:01:55 -0300

    Criação da Classe Hotel e metodos

---
+++ src/Hotel.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class Hotel {
+
+    ArrayList<Quarto> quartos = new ArrayList<>();
+
+    public void adicionarQuarto(Quarto quarto){
+        quartos.add(quarto);
+    }
+
+    public void listarQuarto(){
+        System.out.println("===================");
+        System.out.println("   Lista de Quartos");
+        System.out.println("===================");
+
+        for (int i = 0; i <quartos.size(); i++){
+            System.out.println("Número: " + quartos.get(i).getNumero());
+            System.out.println("Tipo: " + quartos.get(i).getTipo());
+
+            if (quartos.get(i) .getDisponibilidade()){
+                System.out.println("Disponibilidade : Disponível");
+            }else {
+                System.out.println("Disponibilidade : Indisponível");
+            }
+            System.out.println();
+        }
+    }
+
+    public void reservaQuarto(int numeroQuarto){
+        for (int i = 0; i <quartos.size(); i++){
+            if (quartos.get(i).getNumero() == numeroQuarto){
+                if (quartos.get(i).getDisponibilidade()){
+
+                    quartos.get(i).setDisponibilidade(false);
+                    System.out.println("Quarto Reservado!");
+                }else {
+                    System.out.println("Quarto Indisponível");
+                }
+                return;
+            }
+        }
+        System.out.println("Quarto não encontrado!");
+    }
+
+    public void cancelarReserva (int numeroQuarto){
+
+        for (int i = 0; i <quartos.size(); i++){
+
+            if (quartos.get(i).getNumero() == numeroQuarto){
+
+                if (quartos.get(i).getDisponibilidade()){
+                    System.out.println("Quarto não estava reservado!");
+                }else {
+                    quartos.get(i).setDisponibilidade(true);
+                    System.out.println("Reserva cancelada! Quarto disponível.");
+                }
+                return;
+            }
+        }
+        System.out.println("Quarto não encontrado!");
+    }
+}
