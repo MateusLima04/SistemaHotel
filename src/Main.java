d3a5dbe1a7d06e48b0a61978f55f25e1005da3a9
MateusLima04 <mateuslimaa04@gmail.com>
2026-04-12 22:05:08 -0300

    Adicionando opção de menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        boolean executar = true;
+
+        while (executar){
+            System.out.println("===================");
+            System.out.println("   SISTEMA HOTEL   ");
+            System.out.println("===================");
+
+            System.out.println("[1] - Cadastrar Quarto");
+            System.out.println("[2] - Listar Quarto");
+            System.out.println("[3] - Reservar Quarto");
+            System.out.println("[4] - Cancelar Quarto");
+            System.out.println("[5] - Sair");
+
+            int opcao  = scanner.nextInt();
+
+            switch (opcao){
+                case 5:
+                    System.out.println("Programa saindo...");
+                    executar = false;
+                    break;
+            }
+        }
     }
 }
